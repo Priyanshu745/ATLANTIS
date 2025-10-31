55dc7bac87b065381e99e27aa5022b78a45babb9
PRIYANSHU <91508865+Priyanshu745@users.noreply.github.com>
2025-10-31 15:30:43 +0530

    Delete server directory

--- server/config/db.js
+++
@@ -1,9 +0,0 @@
-require("dotenv").config();
-const mongoose = require("mongoose");
-
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log("==============Mongodb Database Connected Successfully=============="))
-  .catch((err) => {
-    console.log("❌ Database Not Connected !!!");
-    console.error(err);
-  });
